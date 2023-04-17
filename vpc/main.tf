0cb9d92f029929a65835b8ed71f23790bfd1b009
Dee <deebio2@yahoo.co.uk>
2023-04-18 00:08:29 +0100

    elearning files

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,3 +1,17 @@
+// configure aws provider to establish a secure connection between terraform and aws
+provider "aws" {
+  region = var.region
+
+  default_tags {
+    tags = {
+      "Automation"  = "terraform"
+      "project"     = var.project_name
+      "environment" = var.environment
+    }
+  }
+}
+
+
 # create vpc
 resource "aws_vpc" "elearning-vpc" {
   cidr_block           = var.elearning-vpc_cidr
@@ -11,7 +25,7 @@ resource "aws_vpc" "elearning-vpc" {
 
 # create internet gateway and attach it to vpc
 resource "aws_internet_gateway" "internet_gateway" {
-  vpc_id = aws_vpc.elearning-vpc.id 
+  vpc_id = aws_vpc.elearning-vpc.id
 
   tags = {
     Name = "elearning-igw"
@@ -23,10 +37,10 @@ data "aws_availability_zones" "available_zones" {}
 
 # create public subnet az1
 resource "aws_subnet" "public_subnet_az1" {
-  vpc_id                  = aws_vpc.elearning-vpc.id  
+  vpc_id                  = aws_vpc.elearning-vpc.id
   cidr_block              = var.public_subnet_az1_cidr
-  availability_zone       = data.aws_availability_zones.available_zones.names[0] 
-  map_public_ip_on_launch = true 
+  availability_zone       = data.aws_availability_zones.available_zones.names[0]
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "elearning-public-az1"
@@ -35,9 +49,9 @@ resource "aws_subnet" "public_subnet_az1" {
 
 # create public subnet az2
 resource "aws_subnet" "public_subnet_az2" {
-  vpc_id                  = aws_vpc.elearning-vpc.id  
+  vpc_id                  = aws_vpc.elearning-vpc.id
   cidr_block              = var.public_subnet_az2_cidr
-  availability_zone       = data.aws_availability_zones.available_zones.names[1] 
+  availability_zone       = data.aws_availability_zones.available_zones.names[1]
   map_public_ip_on_launch = true
 
   tags = {
@@ -47,7 +61,7 @@ resource "aws_subnet" "public_subnet_az2" {
 
 # create route table and add public route
 resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.elearning-vpc.id 
+  vpc_id = aws_vpc.elearning-vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -62,21 +76,21 @@ resource "aws_route_table" "public_route_table" {
 
 # associate public subnet az1 to "public route table"
 resource "aws_route_table_association" "public_subnet_az1_rt_association" {
-  subnet_id      = aws_subnet.public_subnet_az1.id  
-  route_table_id = aws_route_table.public_route_table.id 
+  subnet_id      = aws_subnet.public_subnet_az1.id
+  route_table_id = aws_route_table.public_route_table.id
 }
 
 # associate public subnet az2 to "public route table"
 resource "aws_route_table_association" "public_subnet_2_rt_association" {
-  subnet_id      = aws_subnet.public_subnet_az2.id 
-  route_table_id = aws_route_table.public_route_table.id 
+  subnet_id      = aws_subnet.public_subnet_az2.id
+  route_table_id = aws_route_table.public_route_table.id
 }
 
 # create private a subnet az1
 resource "aws_subnet" "private_subnet_az1" {
-  vpc_id                  = aws_vpc.elearning-vpc.id  
+  vpc_id                  = aws_vpc.elearning-vpc.id
   cidr_block              = var.private_subnet_az1_cidr
-  availability_zone       = data.aws_availability_zones.available_zones.names[0]  
+  availability_zone       = data.aws_availability_zones.available_zones.names[0]
   map_public_ip_on_launch = false
 
   tags = {
@@ -86,9 +100,9 @@ resource "aws_subnet" "private_subnet_az1" {
 
 # create private  subnet az2
 resource "aws_subnet" "private_subnet_az2" {
-  vpc_id                  = aws_vpc.elearning-vpc.id 
+  vpc_id                  = aws_vpc.elearning-vpc.id
   cidr_block              = var.private_subnet_az2_cidr
-  availability_zone       = data.aws_availability_zones.available_zones.names[1]  
+  availability_zone       = data.aws_availability_zones.available_zones.names[1]
   map_public_ip_on_launch = false
 
   tags = {
@@ -98,7 +112,7 @@ resource "aws_subnet" "private_subnet_az2" {
 
 
 resource "aws_route_table" "private_route_table" {
-  vpc_id = aws_vpc.elearning-vpc.id 
+  vpc_id = aws_vpc.elearning-vpc.id
 
   tags = {
     Name = "elearning-private-rt"
@@ -107,14 +121,14 @@ resource "aws_route_table" "private_route_table" {
 
 # associate private subnet az1 to private route table
 resource "aws_route_table_association" "private_subnet_az1_rt_association" {
-  subnet_id      = aws_subnet.private_subnet_az1.id  
-  route_table_id = aws_route_table.private_route_table.id 
+  subnet_id      = aws_subnet.private_subnet_az1.id
+  route_table_id = aws_route_table.private_route_table.id
 }
 
 # associate private subnet az2 to private route table
 resource "aws_route_table_association" "private_subnet_az2_rt_association" {
-  subnet_id      = aws_subnet.private_subnet_az2.id  
-  route_table_id = aws_route_table.private_route_table.id 
+  subnet_id      = aws_subnet.private_subnet_az2.id
+  route_table_id = aws_route_table.private_route_table.id
 }
 
 
