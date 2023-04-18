061b774eb517c6ac327f5e0e7abb5ec7509555bd
Dee <deebio2@yahoo.co.uk>
2023-04-18 08:42:35 +0100

    ekearning file

--- alb/main.tf
+++ alb/main.tf
@@ -1,3 +1,4 @@
-resource "aws_lb" "test" {
- 
+# $ terraform import aws_lb.elearning-lb arn:aws:elasticloadbalancing:eu-west-2:269043264593:loadbalancer/app/elearning-lb/4cb0f4cfe50e7732
+resource "aws_lb" "elearning-lb" {
+  # (resource arguments)
 }
--- ecs/main.tf
+++ ecs/main.tf
@@ -1,3 +1,5 @@
-resource "aws_ecs_cluster" "e-project" {
-  
+# terraform import aws_ecs_cluster.stateless stateless-app
+  # aws_ecs_cluster.elearning-cluster:
+resource "aws_ecs_cluster" "elearning-cluster" {
+    name               = "elearning-cluster"
 }
